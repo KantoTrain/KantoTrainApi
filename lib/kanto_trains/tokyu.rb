0668d748dd0aee43f6680519f5b495b4c546967d
22pop.fm <panpakapan0326@gmail.com>
2020-10-09 02:11:18 +0900

    [fix] class name

--- lib/kanto_trains/tokyu.rb
+++ lib/kanto_trains/tokyu.rb
@@ -1,7 +1,9 @@
-class KantoTrains::Tokyu
-  def self.toyoko_stations 
-    File.open("./data/tokyu/toyoko.json") do |file|
-      JSON.load(file)
-    end
+class Tokyu
+  class Toyoko
+    def self.stations 
+      File.open("./data/tokyu/toyoko.json") do |file|
+        JSON.load(file)
+      end
+    end  
   end
 end
